4bc6b80c5ce6d22519be4b8f03243f003b421935
AndriiHryhorovych <59657519+AndriiHryhorovych@users.noreply.github.com>
2021-09-20 23:03:01 +0300

    delivery, paymants added

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -3,24 +3,24 @@
         <div class="container">
             <div class="row">
                 <div class="col-2 title">
-                    <router-link to="/">eShop</router-link>
+                    <router-link to="/" class="title-link">eShop</router-link>
                 </div>
                 <div class="col-4">
 
                 </div>
                 <div class="col-6">
-                    <ul class="menu d-flex justify-content-between">
+                    <ul class="menu d-flex justify-content-between align-items-center">
                         <li>
-                            <router-link to="/catalog">Каталог</router-link>
+                            <router-link to="/catalog" class="menu-item">Каталог</router-link>
                         </li>
                         <li>
-                            <router-link to="/payments">Оплата</router-link>
+                            <router-link to="/payments" class="menu-item">Оплата</router-link>
                         </li>
                         <li>
-                            <router-link to="/delivery">Доставка</router-link>
+                            <router-link to="/delivery" class="menu-item">Доставка</router-link>
                         </li>
                         <li>
-                            <router-link to="/cart">Корзина</router-link>
+                            <router-link to="/cart" class="menu-item">Корзина</router-link>
                         </li>
                     </ul>
                 </div>
@@ -41,7 +41,33 @@
     color: white;
     height: 60px;
 }
-.title {
-    font-size: 28px;
+.title-link {
+    text-decoration: none;
+    font-size: 32px;
+    color: white;
+    transition: 0.3s;
+}
+.title-link:hover {
+    color: gray;
+    transition: 0.3s;
+}
+.menu a{
+    text-decoration: none;
+    color: white;
+    font-size: 16px;
+    transition: 0.3s;
+}
+.menu a:hover{
+    color: gray;
+    transition: 0.3s;
+    
+}
+.menu li{
+    display: flex;
+    align-items: center;
+}
+.menu{
+     margin: 0 0;
+     padding: 0 16px;
 }
 </style>
