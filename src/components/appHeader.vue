b96a19bdbcfa9ba0c77a179601fe4c794f46038c
AndriiHryhorovych <59657519+AndriiHryhorovych@users.noreply.github.com>
2021-09-20 17:09:43 +0300

    menu added

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -3,13 +3,26 @@
         <div class="container">
             <div class="row">
                 <div class="col-2 title">
-                    <span>eShop</span>
+                    <router-link to="/">eShop</router-link>
                 </div>
                 <div class="col-4">
 
                 </div>
                 <div class="col-6">
-
+                    <ul class="menu d-flex justify-content-between">
+                        <li>
+                            <router-link to="/catalog">Каталог</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/payments">Оплата</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/delivery">Доставка</router-link>
+                        </li>
+                        <li>
+                            <router-link to="/cart">Корзина</router-link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-
+        <h2>Каталог</h2>
     </div>
 </template>
 
